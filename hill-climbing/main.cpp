583c0ae314ceb8eab051cf48830ea90001e13071
s22442 <s22442@pjwstk.edu.pl>
2023-03-11 20:27:15 +0100

    feat(hill-climbing): initial data

--- hill-climbing/main.cpp
+++ hill-climbing/main.cpp
@@ -1,4 +1,32 @@
 #include <iostream>
+#include <array>
+#include <vector>
+
+const auto BIN_WEIGHT_LIMIT = 100;
+
+class GarbageBag {
+private:
+    int weight;
+
+public:
+    explicit GarbageBag(const int weight) {
+        this->weight = weight;
+    }
+
+    auto getWeight() const {
+        return this->weight;
+    }
+};
+
+const auto GARBAGE_BAGS = std::array{
+        GarbageBag(50),
+        GarbageBag(20),
+        GarbageBag(40),
+        GarbageBag(70),
+        GarbageBag(10),
+        GarbageBag(30),
+        GarbageBag(80)
+};
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
