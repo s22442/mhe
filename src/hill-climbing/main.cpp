532c527fe64150a7f634d7ab4934d61cdbedad8e
brofrain <downloading130@gmail.com>
2023-03-18 11:25:01 +0100

    feat(hill-climbing): best neighbor

--- src/hill-climbing/main.cpp
+++ src/hill-climbing/main.cpp
@@ -5,7 +5,19 @@
 
 const auto BIN_WEIGHT_LIMIT = 100;
 
+// best possible bin count: 8
 const auto GARBAGE_BAGS = std::vector<GarbageBag>{
+    GarbageBag(48),
+    GarbageBag(30),
+    GarbageBag(36),
+    GarbageBag(19),
+    GarbageBag(36),
+    GarbageBag(27),
+    GarbageBag(42),
+    GarbageBag(42),
+    GarbageBag(36),
+    GarbageBag(24),
+    GarbageBag(30),
     GarbageBag(48),
     GarbageBag(30),
     GarbageBag(36),
@@ -21,15 +33,17 @@ const auto GARBAGE_BAGS = std::vector<GarbageBag>{
 
 std::random_device rd;
 std::mt19937 rgen(rd());
-
 const auto garbage_bag_count = GARBAGE_BAGS.size();
+std::uniform_int_distribution<int> distr(0, garbage_bag_count - 1);
 
 class SolutionFactory {
 private:
-    auto swap_random_adjacent_bag_pair(Solution solution) {
-        std::uniform_int_distribution<int> distr(0, garbage_bag_count - 1);
+    auto generate_random_bag_index() {
+        return distr(rgen);
+    }
 
-        auto random_index = distr(rgen);
+    auto swap_random_adjacent_bag_pair(Solution solution) {
+        auto random_index = this->generate_random_bag_index();
         auto next_index = (random_index + 1) % garbage_bag_count;
 
         solution.swap_garbage_bags(random_index, next_index);
@@ -37,8 +51,29 @@ private:
         return solution;
     }
 
+    auto swap_random_bag_with_best_neighbor(Solution solution) {
+        auto random_index = this->generate_random_bag_index();
+
+        auto best_solution = solution;
+
+        for (auto i : range(garbage_bag_count)) {
+            if (i == random_index) {
+                continue;
+            }
+
+            auto new_solution = solution;
+            new_solution.swap_garbage_bags(random_index, i);
+
+            if (new_solution.get_filled_bin_count() < best_solution.get_filled_bin_count()) {
+                best_solution = new_solution;
+            }
+        }
+
+        return best_solution;
+    }
+
 public:
-    auto generate_hillclimbing_solution() {
+    auto generate_random_hillclimbing_solution() {
         auto best_solution = Solution{BIN_WEIGHT_LIMIT, GARBAGE_BAGS};
 
         while (true) {
@@ -53,14 +88,40 @@ public:
 
         return best_solution;
     }
+
+    auto generate_best_neighbor_hillclimbing_solution() {
+        auto best_solution = Solution{BIN_WEIGHT_LIMIT, GARBAGE_BAGS};
+
+        while (true) {
+            auto new_solution = swap_random_bag_with_best_neighbor(best_solution);
+
+            if (new_solution.get_filled_bin_count() < best_solution.get_filled_bin_count()) {
+                best_solution = new_solution;
+            } else {
+                break;
+            }
+        }
+
+        return best_solution;
+    }
 };
 
 int main() {
     auto solution_factory = SolutionFactory{};
 
-    auto hillclimbing_solution = solution_factory.generate_hillclimbing_solution();
+    std::cout
+        << "Random hill climbing solution: "
+        << std::endl
+        << solution_factory.generate_random_hillclimbing_solution()
+        << std::endl;
 
-    std::cout << "Hill climbing solution: " << hillclimbing_solution << std::endl;
+    std::cout << std::endl;
+
+    std::cout
+        << "Best neighbor hill climbing solution: "
+        << std::endl
+        << solution_factory.generate_best_neighbor_hillclimbing_solution()
+        << std::endl;
 
     return 0;
 }
